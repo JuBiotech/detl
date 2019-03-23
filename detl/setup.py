e679cd4d4be37ce7a41d7b0958c9da12c3f1f937
Michael Osthege <m.osthege@fz-juelich.de>
2019-03-23 14:59:59 +0100

    adds initially empty project files closes #2

---
+++ detl/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+__packagename__ = 'detl'
+
+def get_version():
+    import os, re
+    VERSIONFILE = os.path.join(__packagename__, '__init__.py')
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+__version__ = get_version()
+
+
+setuptools.setup(name = __packagename__,
+        packages = setuptools.find_packages(), # this must be the same as the name above
+        version=__version__,
+        description='Package for parsing and transforming BioLector raw data.',
+        url='https://gitlab.fz-juelich.de/IBG-1/micropro/detl',
+        download_url = 'https://gitlab.fz-juelich.de/IBG-1/micropro/detl/tarball/%s' % __version__,
+        author='IBG-1',
+        copyright='(c) 2019 Forschungszentrum Jülich GmbH',
+        license='(c) 2019 Forschungszentrum Jülich GmbH',
+        classifiers= [
+            'Programming Language :: Python',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Intended Audience :: Developers'
+        ],
+        install_requires=[
+            'pandas'
+        ]
+)
+
---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+"""Contains unit tests for the `detl` package"""
+import numpy
+import pathlib
+import pandas
+import unittest
+
+import detl
+
+
