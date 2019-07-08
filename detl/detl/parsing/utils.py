b7a08e073eff42d911f958ecaae7cc6dd286fcf6
Michael Osthege <m.osthege@fz-juelich.de>
2019-07-08 15:49:52 +0200

    treat all DASWARE timestamps as UTC closes #27

--- detl/detl/parsing/utils.py
+++ detl/detl/parsing/utils.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-def dwtimestamp_to_utc(dwtimestamp:str, timeshift_to_utc_in_min:float) -> datetime.datetime:
+def dwtimestamp_to_utc(dwtimestamp:str) -> datetime.datetime:
     dt = datetime.datetime.strptime(dwtimestamp, '%Y-%m-%d %H:%M:%S')
-    dt = dt.replace(tzinfo=datetime.timezone(datetime.timedelta(minutes=-timeshift_to_utc_in_min)))
+    dt = dt.replace(tzinfo=datetime.timezone.utc)
     return dt.astimezone(datetime.timezone.utc)
